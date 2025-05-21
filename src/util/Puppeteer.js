960de9fec9ecf5e65b12edd73ba6bf66ba2d9226
Volve <39830104+volveezz@users.noreply.github.com>
2025-05-21 07:31:34 +0200

    feat: upgrade exposeFunction method

--- src/util/Puppeteer.js
+++ src/util/Puppeteer.js
@@ -1,21 +1,15 @@
 /**
  * Expose a function to the page if it does not exist
- *
- * NOTE:
- * Rewrite it to 'upsertFunction' after updating Puppeteer to 20.6 or higher
- * using page.removeExposedFunction
- * https://pptr.dev/api/puppeteer.page.removeExposedFunction
- *
- * @param {import(puppeteer).Page} page
+
+ * @param {import('puppeteer').Page} page
  * @param {string} name
  * @param {Function} fn
  */
 async function exposeFunctionIfAbsent(page, name, fn) {
-    const exist = await page.evaluate((name) => {
-        return !!window[name];
-    }, name);
-    if (exist) {
-        return;
+    try {
+        await page.removeExposedFunction(name);
+    } catch (err) {
+        //
     }
     await page.exposeFunction(name, fn);
 }
